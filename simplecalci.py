88a77d877dd9fd33da9b88f62e24401d73c90cb7
anonymous <78273014+noobforanonymous@users.noreply.github.com>
2021-02-03 19:42:00 +0530

    Update simplecalci.py

--- simplecalci.py
+++ simplecalci.py
@@ -1,2 +1,22 @@
+from pyfiglet import Figlet
+
+green = '\033[1;92m'
+red = '\033[31m'
+blue = '\033[34m'
+clear = '\033[0m'
+bold = '\033[01m'
+
+print(green + """
+ _____ _______   _    _          _____ _  __ _____ 
+|  __ \__   __| | |  | |   /\   / ____| |/ // ____|
+| |__) | | |    | |__| |  /  \ | |    | ' /| (___  
+|  _  /  | |    |  __  | / /\ \| |    |  <  \___ \ 
+| | \ \  | |    | |  | |/ ____ \ |____| . \ ____) |
+|_|  \_\ |_|    |_|  |_/_/    \_\_____|_|\_\_____/ 
+""" + green)
+print(red + bold + "   <====YOUTUBE CHANNEL NAME : RTHACKS====> \n" + clear)
+print(blue + bold + " <=========https://dev-regaanthamimprogramming.pantheonsite.io/=======> \n" + clear)
+
+
 rthacks = input("YOUR EXPRESSION : ")
 print("ANSWER : " , eval(rthacks))
